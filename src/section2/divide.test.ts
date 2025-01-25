61439427a2a3e46e52b426251da29b49bcd03677
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-25 12:58:02 +0900

    エラーメッセージ以外がスローされた時にテストに失敗、ZeroDivisorError(特定のエラーが発生している時)が発生し、その他のエラーがスローされた時に失敗する

--- src/section2/divide.test.ts
+++ src/section2/divide.test.ts
@@ -3,5 +3,7 @@
 import { divide,ZeroDivisorError} from "./divide";
 
 it("0で割るとエラーが発生する",()=> {
-    expect(() => divide(10,1)).toThrow();
+    expect(() => divide(10,0)).toThrow();
+    expect(() => divide(10,0)).toThrow("0で割ることはできません");
+    expect(() => divide(10,0)).toThrow(ZeroDivisorError);
 });
