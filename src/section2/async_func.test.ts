fac02ed477b8fca0a957e93e10736e8d9596f5f6
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-01-28 22:51:15 +0900

    プロミスの処理が失敗するケースのテスト

--- src/section2/async_func.test.ts
+++ src/section2/async_func.test.ts
@@ -4,3 +4,12 @@ it("delayが指定された時間後にメッセージを返す",async ()=> {
     const result = await delay("Jestの練習だ",1000);
     expect(result).toBe("Jestの練習だ");
 });
+
+//プロミスの処理が失敗するケース
+it("timeがマイナスの場合はエラーが発生する",async ()=> {
+    try {
+        const result = await delay("Jestの練習だ",-1);
+    } catch(e: any) {
+        expect(e.message).toBe("timeは0以上で指定してください");
+    }
+});
